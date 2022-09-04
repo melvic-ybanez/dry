8b8ad725a24627b5eb547170235958cfe16dfaf6
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2022-09-05 02:01:47 +0800

    Remove unnecessary AST nodes

--- src/main/scala/com/melvic/dry/ast/Expr.scala
+++ src/main/scala/com/melvic/dry/ast/Expr.scala
@@ -18,20 +18,4 @@ object Expr {
   final case class Grouping(expr: Expr) extends Expr
   final case class Unary(operator: Token, operand: Expr) extends Expr
   final case class Binary(left: Expr, operator: Token, right: Expr) extends Expr
-
-  sealed trait Operator extends Expr
-
-  object Operator {
-    case object EqualEqual extends Operator
-    case object NotEqual extends Operator
-    case object LessThan extends Operator
-    case object LessThanEqual extends Operator
-    case object GreaterThan extends Operator
-    case object GreaterThanEqual extends Operator
-    case object Plus extends Operator
-    case object Minus extends Operator
-    case object Multiply extends Operator
-    case object Divide extends Operator
-    case object Modulo extends Operator
-  }
 }
