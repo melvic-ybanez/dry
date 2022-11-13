df2d0454d97d7be919625bc18cf5e62448976a23
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2022-11-13 17:11:51 +0800

    Add assert function

--- src/main/scala/com/melvic/dry/interpreter/Interpreter.scala
+++ src/main/scala/com/melvic/dry/interpreter/Interpreter.scala
@@ -1,12 +1,14 @@
 package com.melvic.dry.interpreter
 
 import com.melvic.dry.ast.Decl
+import com.melvic.dry.aux.Show.ShowInterpolator
 import com.melvic.dry.interpreter.Env.LocalEnv
 import com.melvic.dry.interpreter.eval.{EvalOut, Evaluate}
 import com.melvic.dry.interpreter.values.Callable
 import com.melvic.dry.interpreter.values.Value.{Bool, Num, Str, ToValue}
 import com.melvic.dry.resolver.Locals
 import com.melvic.dry.result.Result
+import com.melvic.dry.result.Result.implicits.ToResult
 
 object Interpreter {
   def interpret(declarations: List[Decl], enclosing: Env, locals: Locals): EvalOut = {
@@ -39,6 +41,14 @@ object Interpreter {
         case Str(_)      => Str("string")
         case Value.Unit  => Str("unit")
         case _: Callable => Str("function")
+      }
+    )
+    .defineWith(
+      "assert",
+      Callable(3, _) { case arg1 :: arg2 :: message :: _ =>
+        if (arg1 == arg2) println(show"${Console.GREEN}[Successful] $message")
+        else System.err.println(show"[Failure] Expected: $arg1. Got: $arg2")
+        Value.unit.ok
       }
     )
 }
