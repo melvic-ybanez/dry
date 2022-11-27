8833d71e5c7e6d6d04ef5173be98d1f2b228c7a3
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2022-11-28 04:52:41 +0800

    Fix adding of semicolon after } in repl

--- src/main/scala/com/melvic/dry/interpreter/Run.scala
+++ src/main/scala/com/melvic/dry/interpreter/Run.scala
@@ -20,7 +20,8 @@ object Run {
           ".",
           // this is a trick I'm using to make repl accept expressions that
           // do not end with semicolons.
-          if (input.endsWith(";")) input else s"println(str($input));",
+          if (!input.endsWith(";")  && !input.endsWith("}")) input + ";"
+          else input,
           env
         )
         .map { case (value, _) =>
