6fbf7d2a02ca70cc00094c177407c4c54b4d1f41
Melvic Ybanez <melvic@hubbub.ai>
2023-10-12 02:01:04 +0800

    Prioritize errors that occur at the later stages

--- src/main/scala/com/melvic/dry/interpreter/Repl.scala
+++ src/main/scala/com/melvic/dry/interpreter/Repl.scala
@@ -3,6 +3,7 @@ package com.melvic.dry.interpreter
 import com.melvic.dry.aux.Nel
 import com.melvic.dry.resolver.Scopes
 import com.melvic.dry.result.Failure
+import com.melvic.dry.result.Failure.{LexerError, Line, ParseError, ResolverError, RuntimeError}
 
 import scala.io.StdIn.readLine
 import scala.util.chaining.scalaUtilChainingOps
@@ -28,10 +29,10 @@ trait Repl {
       writeSuccess(Value.Str("Bye!"))
       exit()
     } else {
-      def runExpression(scriptFailures: Nel[Failure]): Unit =
+      def runExpression(scriptErrors: Nel[Failure]): Unit =
         Interpret.expression(input, env, scopes) match {
-          case Left(exprFailures) =>
-            writeFailures((exprFailures ++ scriptFailures).toList.distinct)
+          case Left(exprErrors) =>
+            writeFailures(prioritizeErrors(exprErrors ++ scriptErrors))
             continue(env, scopes)
           case Right(value) =>
             writeSuccess(value)
@@ -52,6 +53,26 @@ trait Repl {
 
       runScript()
     }
+
+  /**
+   * Prioritizes the errors that occurred at the later stages of the interpreter pipeline. This is only needed
+   * because right now, the REPL interprets the input in multiple ways, and we want to report the erroneous
+   * paths that have gone the furthest. After all, the more stages the input has passed, the closer it is to
+   * achieving legitimacy.
+   */
+  private def prioritizeErrors(errors: Nel[Failure]): List[Failure] = {
+    val allErrors = errors.toList.distinct
+    val priorities = allErrors.map {
+      case _: RuntimeError  => 1
+      case _: ResolverError => 2
+      case _: ParseError    => 3
+      case _: LexerError    => 4
+      case _: Line          => 5
+    }
+    val priorityTable = allErrors.zip(priorities).groupBy(_._2)
+    val topPrio = priorityTable.toList.minBy(_._1)
+    topPrio._2.map(_._1)
+  }
 }
 
 object Repl {
@@ -60,7 +81,7 @@ object Repl {
       println(s"${Console.GREEN}${Value.show(value)}${Console.RESET}")
 
     override def continue(env: Env, scopes: Scopes): Unit = {
-      val input = readLine(s"${Console.YELLOW}dry> ${Console.RESET}")
+      val input = readLine(s"${Console.MAGENTA}dry> ${Console.RESET}")
       start(input, env, scopes)
     }
 
