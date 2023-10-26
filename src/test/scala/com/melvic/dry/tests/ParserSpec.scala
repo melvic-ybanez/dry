4596d2e4d6e0fdafa7543b3e648a96cd4d4c1cce
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2023-10-26 21:01:58 +0800

    Add test case for assigning values to 'self'

--- src/test/scala/com/melvic/dry/tests/ParserSpec.scala
+++ src/test/scala/com/melvic/dry/tests/ParserSpec.scala
@@ -10,6 +10,13 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
+/**
+ * Houses the test cases for the parsing phase that are difficult to implement using Dry's tests (because the
+ * parser would fail in the first place).
+ *
+ * Note: many of the test cases here are based on the lox test suite:
+ * https://github.com/munificent/craftinginterpreters/blob/master/test/
+ */
 class ParserSpec extends AnyFlatSpec with should.Matchers with ScalaCheckPropertyChecks {
   "Return statement" should "end in a semicolon" in {
     val noSemicolon = "let sum = lambda(x, y) { return x + y };"
@@ -22,21 +29,30 @@ class ParserSpec extends AnyFlatSpec with should.Matchers with ScalaCheckPropert
 
   "Assignment target" should "not be a paren-enclosed var" in {
     val error = interpret("""let a = "a"; (a) = "value";""")
-    error should matchPattern { case Left(One(InvalidAssignmentTarget(_))) => }
+    assertInvalidAssignmentTarget(error)
   }
 
   it should "not be an arithmetic expression" in {
     val error = interpret("""let a = "a"; let b = "b"; a + b = "value";""")
-    error should matchPattern { case Left(One(InvalidAssignmentTarget(_))) => }
+    assertInvalidAssignmentTarget(error)
   }
 
   it should "not be an expression with prefix operator" in {
     val error = interpret("""let a = "a"; !a = "value";""")
-    error should matchPattern { case Left(One(InvalidAssignmentTarget(_))) => }
+    assertInvalidAssignmentTarget(error)
+  }
+
+  it should "not work with 'self' as the lhs" in {
+    val error = interpret("""class Foo { def foo() { self = "value"; }}""")
+    assertInvalidAssignmentTarget(error)
   }
 
   def assertSuccess(result: Result[Value]): Unit =
     result should be(Right(Value.Unit))
+
+  def assertInvalidAssignmentTarget(error: Result[Value]): Unit = {
+    error should matchPattern { case Left(One(InvalidAssignmentTarget(_))) => }
+  }
 }
 
 object ParserSpec {
